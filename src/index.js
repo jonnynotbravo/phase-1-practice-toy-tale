3c314bd0359a5cd2d0fa31c1a6795c1949bf5d4c
Leah Cardoz <leahcardoz@gmail.com>
2022-10-18 11:48:14 -0400

    appended tags to div, appended div to toycollection, fixed event listener so that number of likes updates when button is clicked

--- src/index.js
+++ src/index.js
@@ -1,66 +1,56 @@
 let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
-
-
+document.addEventListener('DOMContentLoaded', () => {
+	const addBtn = document.querySelector('#new-toy-btn');
+	const toyFormContainer = document.querySelector('.container');
+	addBtn.addEventListener('click', () => {
+		// hide & seek with the form
+		addToy = !addToy;
+		if (addToy) {
+			toyFormContainer.style.display = 'block';
+		} else {
+			toyFormContainer.style.display = 'none';
+		}
+	});
 });
 
 const toyCollection = document.querySelector('#toy-collection');
 
-
-
-
 function getImage() {
-
-  fetch(' http://localhost:3000/toys')
-  .then(response => response.json())
-   .then(data => renderImages(data));
-
+	fetch(' http://localhost:3000/toys')
+		.then((response) => response.json())
+		.then((data) => renderImages(data));
 }
 
 function renderImages(images) {
+	images.forEach((elem) => {
+		const card = document.createElement('div');
+		card.className = 'card';
 
-   images.forEach(elem => {
-    const card = document.createElement('div');
-    card.className = 'card';
-    
-    const img = document.createElement('img');
-    img.src = elem.image;
-   
-    const toyName = document.createElement('h2');
-    toyName.innerText = elem.name;
-    
-    const numLikes = document.createElement('p');
-    numLikes.innerText = elem.likes + ' Likes';
+		const img = document.createElement('img');
+		img.src = elem.image;
 
-    const btn = document.createElement('button');
-    btn.className = 'like-btn';
-    btn.id = `${elem.id}`
-    btn.innerText = 'Like ❤️';
-    btn.addEventListener('click', () => {
-      elem.likes ++
-    })
-    
+		const toyName = document.createElement('h2');
+		toyName.textContent = elem.name;
 
-    //card.appendChild(img);
+		const numLikes = document.createElement('p');
+		numLikes.innerText = elem.likes + ' Likes';
 
-    // toyCollection.append(img);
-    
-    
-
- })
-
+		const btn = document.createElement('button');
+		btn.className = 'like-btn';
+		btn.id = `${elem.id}`;
+		btn.innerText = 'Like ❤️';
+		btn.addEventListener('click', () => {
+			numLikes.innerText = elem.likes++ + ' Likes';
+		});
+		//need to append img, toyName, numLikes, btn to div card
+		//then need to append div card to toyCollection
+		card.appendChild(img);
+		card.appendChild(toyName);
+		card.appendChild(numLikes);
+		card.appendChild(btn);
+		toyCollection.appendChild(card);
+	});
 }
 
 getImage();
